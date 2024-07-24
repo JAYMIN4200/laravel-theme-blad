1898b3e7eaae3e01c7924afe3acd9a47c96d15ef
Jaymin Panchal <121655018+JAYMIN4200@users.noreply.github.com>
2024-07-24 11:01:18 +0530

    Update 404.blade.php

--- resources/views/frontend/404.blade.php
+++ resources/views/frontend/404.blade.php
@@ -3,42 +3,43 @@
 @push('title')
 <title>404 - Error Page</title>
 @endpush
+
 @section('content')
 
-  <!-- breadcrumb start -->
-  <div class="breadcrumb_section overflow-hidden ptb-150 error_bread">
+<!-- breadcrumb start -->
+<div class="breadcrumb_section overflow-hidden ptb-150 error_bread">
     <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-xl-6 col-lg-6 col-md-8 col-sm-10 col-12 text-center">
-          <h2>Error 404</h2>
-          <ul>
-            <li><a href="{{route('home1')}}">Home</a></li>
-            <li class="active">404</li>
-          </ul>
+        <div class="row justify-content-center">
+            <div class="col-xl-6 col-lg-6 col-md-8 col-sm-10 col-12 text-center">
+                <h2>Error 404</h2>
+                <ul>
+                    <li><a href="{{ route('home1') }}">Home</a></li>
+                    <li class="active">404</li>
+                </ul>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-  <!-- breadcrumb end -->
+</div>
+<!-- breadcrumb end -->
 
-  <!-- 404 start -->
-  <section class="error ptb-115">
+<!-- 404 start -->
+<section class="error ptb-115">
     <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-xl-6 col-lg-7 col-md-9 col-sm-10 col-12">
-          <div class="error_detail text-center">
-            <span class="mb-1 mb-xl-4 mb-lg-4 mb-md-3 mb-sm-2">404</span>
-            <h1 class="mb-4 mb-xl-5">Page Not Found</h1>
-            <p class="mb-4 mb-xl-5">
-              The page you are looking for might have been removed had its
-              name changed or is temporarily unavailable
-            </p>
-            <a href="{{route('home1')}}" class="red_btn explore_now">Back To Home</a>
-          </div>
+        <div class="row justify-content-center">
+            <div class="col-xl-6 col-lg-7 col-md-9 col-sm-10 col-12">
+                <div class="error_detail text-center">
+                    <span class="mb-1 mb-xl-4 mb-lg-4 mb-md-3 mb-sm-2">404</span>
+                    <h1 class="mb-4 mb-xl-5">Page Not Found</h1>
+                    <p class="mb-4 mb-xl-5">
+                        The page you are looking for might have been removed, had its
+                        name changed, or is temporarily unavailable.
+                    </p>
+                    <a href="{{ route('home1') }}" class="red_btn explore_now">Back To Home</a>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </section>
-  <!-- 404 end -->
+</section>
+<!-- 404 end -->
 
 @endsection
